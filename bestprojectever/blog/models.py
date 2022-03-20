9ddf770f0cef3f5a513b0e2d245d041716dc33b3
JakubWronka <jakubwronka7@gmail.com>
2022-03-20 11:09:16 +0100

    Create first version of a CustomUser model

--- bestprojectever/blog/models.py
+++ bestprojectever/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_account_creation = models.DateField(auto_now_add=True)  # to discuss, might not be needed
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    # photo - field that stores files = to discuss
+    # how should we handle Friends field/list
