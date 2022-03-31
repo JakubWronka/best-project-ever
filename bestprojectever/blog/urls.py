e3d027f29b1a10763081ed1a811703a1c0cd839c
PythonMaciej <Python.Maciej.Code@gmail.com>
2022-03-31 20:06:44 +0200

    Change the view approach (changed to classes)

--- bestprojectever/blog/urls.py
+++ bestprojectever/blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from . import views
+from .views import ArticleDetailView, ArticleListView
 
 # każda aplikacja ma swoją konfigurację url
 # oprócz dodania tutaj urlpatterns, 
 # trzeba to zaimportować do głównego url configuration (urls.py w głównym folderze)
 urlpatterns = [
     # poniżej dodaję co ma wywoływać django jeżeli do adresu na końcu dodam "articles/"
-    path('articles/', views.articles_view)
+    path('articles/', ArticleListView.as_view()),
+    path('articles/<int:pk>', ArticleDetailView.as_view()),
 ]
