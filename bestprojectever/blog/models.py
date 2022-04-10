3bd621ca87bf3856d24637e078ba3dfb85f25c68
JakubWronka <jakubwronka7@gmail.com>
2022-04-10 11:50:14 +0200

    Redifine CustomUser class and register it in the admin panel

--- bestprojectever/blog/models.py
+++ bestprojectever/blog/models.py
@@ -1,13 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 class CustomUser(AbstractUser):
+    username = models.CharField(blank=True, null=True, max_length=50)
+    # Overwrtiting to available having a blank username, because the email field will be the main field 
+    # used for authentication, for easier maintenanace
+    email = models.EmailField('email address', unique=True)  
+    # We have to overwrite this field from an AbstractUser model, 
+    # to make the 'unique' constraint - so it will be available for using for the authentication
+    USERNAME_FIELD = 'email'
+    # That means that this field will be used for authentication
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    # Fields required by 'createsuperuser' command, username has to be there, otherwise Django will complain
+
+    def __str__(self):
+        return "{}".format(self.email)
+
+class CustomUserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile')
     date_of_birth = models.DateField(null=True, blank=True)
-    date_of_account_creation = models.DateField(auto_now_add=True)  # TODO - to delete, there is a 'date_joined' field in the above AbstractUser class
+    country = models.CharField(max_length=50, blank=True)
+    city = models.CharField(max_length=50, blank=True)
     bio = models.TextField(max_length=500, blank=True)
-    location = models.CharField(max_length=30, blank=True)
-    # photo - field that stores files = to discuss
-    # how should we handle Friends field/list
+    photo = models.ImageField(upload_to='uploads', blank=True)
 
 # Utworzenie modelu Article dla posta/wpisu na blogu
 class Article(models.Model):
