2af8e9f47afabf9eef69bc6518d548eca5453218
PythonMaciej <53489801+PythonMaciej@users.noreply.github.com>
2022-05-07 12:26:37 +0200

    Docker 2 (#7)
    * dockerignore and dockerization
    
    * asd
    
    * Initial state of database added

---
+++ bestprojectever/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+echo "Inside docker-entrypoint script"
+
+python manage.py makemigrations
+python manage.py migrate
+# to have the same initial state of database it will be loaded from file "dumped_db.json":
+echo "In a moment initial state of database will be loaded"
+python manage.py loaddata dumped_db.json
+echo "Initial state of database from file 'dumped_db.json has been loaded"
+python manage.py runserver 0.0.0.0:8000
+
+
+echo "Finishing docker-entrypoint script"
