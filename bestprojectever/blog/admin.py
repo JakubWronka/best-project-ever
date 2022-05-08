0cdc6663dd5a52c2a6ee09cbe1010ec0c5e04dcf
Jakub Wronka <jakubwronka7@gmail.com>
2022-05-08 07:42:19 +0200

    User management (#8)
    * Add login option to the browsable API
    
    * Redifine CustomUser class and register it in the admin panel
    
    * Redifine serializers for CustomUser model
    
    * Add a comment for a future development

--- bestprojectever/blog/admin.py
+++ bestprojectever/blog/admin.py
@@ -1,5 +1,38 @@
 from django.contrib import admin
-from .models import CustomUser, Article
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, CustomUserProfile, Article
 
-admin.site.register(CustomUser)
+# Created superuser: 
+# Email address: jakub@wronka.pl
+# Username: jakub1
+# First name: Jakub
+# Last name: Wronka
+# Password: james
+
+class CustomUserProfileInline(admin.StackedInline):
+    # This class is created to display the CustomUserProfile class within (inline) the CustomUser class in the admin
+    model = CustomUserProfile
+    can_delete = False
+    # That is set because it is not preferrable to delete the CustomUserProfile through the CustomUser
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    inlines = (CustomUserProfileInline, )
+
 admin.site.register(Article)
