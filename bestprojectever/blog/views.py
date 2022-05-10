6f0992f9a16f9740fa98e9705fe3b63b60ded4a5
JakubWronka <jakubwronka7@gmail.com>
2022-05-10 19:34:56 +0200

    Fix requirements, create new data, first attempt on custom templates

--- bestprojectever/blog/views.py
+++ bestprojectever/blog/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, renderers
+from rest_framework.response import Response
 from .models import CustomUser, Article
 from .serializers import CustomUserSerializer, ArticleSerializer
 
@@ -9,3 +10,12 @@ class CustomUserViewSet(viewsets.ModelViewSet):
 class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
+    '''
+    renderer_classes = (renderers.JSONRenderer, renderers.TemplateHTMLRenderer)
+
+    def list(self, request, *args, **kwargs):
+        response = super(ArticleViewSet, self).list(request, *args, **kwargs)
+        if request.accepted_renderer.format == 'html':
+            return Response({'data': response.data}, template_name='blog/article_list.html')
+        return response
+    '''
