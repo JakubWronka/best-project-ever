1126d44e012bc1b898a5bfd869c5043d9172d2d8
JakubWronka <jakubwronka7@gmail.com>
2022-05-21 20:19:18 +0200

    Add custom view templates for article

--- bestprojectever/blog/views.py
+++ bestprojectever/blog/views.py
@@ -10,12 +10,13 @@ class CustomUserViewSet(viewsets.ModelViewSet):
 class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
-    '''
     renderer_classes = (renderers.JSONRenderer, renderers.TemplateHTMLRenderer)
-
+    
     def list(self, request, *args, **kwargs):
         response = super(ArticleViewSet, self).list(request, *args, **kwargs)
-        if request.accepted_renderer.format == 'html':
-            return Response({'data': response.data}, template_name='blog/article_list.html')
-        return response
-    '''
+        #if request.accepted_renderer.format == 'html':
+        return Response({'article_list': response.data}, template_name='blog/article_list.html')
+
+    def retrieve(self, request, pk=None, *args, **kwargs):
+        response = super(ArticleViewSet, self).retrieve(request, pk, *args, **kwargs)
+        return Response({'article': response.data}, template_name='blog/article_detail.html')
