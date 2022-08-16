68587eb6d8b0133565831c0356cdc86f506f8d88
JakubWronka <jakubwronka7@gmail.com>
2022-08-16 19:03:41 +0200

    Delete the url pattern to logging in via REST

--- bestprojectever/bestprojectever/urls.py
+++ bestprojectever/bestprojectever/urls.py
@@ -21,6 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),  # including all URL setup from blog/urls.py file, available under blog/ link
     path('__debug__/', include(debug_toolbar.urls)),
-    path('api-auth/', include('rest_framework.urls')),
     path('accounts/', include('allauth.urls')),
 ]
